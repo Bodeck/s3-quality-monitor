e4c885325760ae0a1ef2c18a2f51a1fc1986bab8
Bogusław Franczak <bfranczak@gmail.com>
2026-03-29 17:36:26 +0200

    Refactor s3 module

--- monitor/s3.py
+++ monitor/s3.py
@@ -1,36 +1,37 @@
 import boto3
 from typing import Generator
 
-
 def list_s3_files(
     bucket: str,
     prefix: str,
     extensions: tuple[str, ...] = (".csv", ".parquet"),
+    s3_client = None
 ) -> Generator[str, None, None]:
-    s3 = boto3.client("s3")
+    client = s3_client or boto3.client("s3")
     kwargs = {"Bucket": bucket, "Prefix": prefix}
 
     while True:
-        response = s3.list_objects_v2(**kwargs)
+        response = client.list_objects_v2(**kwargs)
 
         for obj in response.get("Contents", []):
             key = obj["Key"]
             if key.endswith(extensions):
                 yield key
 
-        next_token = response.get("NextContinuationToken", None)
+        next_token = response.get("NextContinuationToken")
         if next_token is None:
             return
         kwargs["ContinuationToken"] = next_token
 
 
 def list_s3_files_with_paginator(
-    bucket: str, prefix: str, extentions: tuple[str, ...] = (".csv", ".parquet")
+    bucket: str, prefix: str, extensions: tuple[str, ...] = (".csv", ".parquet"), s3_client = None
 ) -> Generator[str, None, None]:
-    s3 = boto3.client("s3")
-    paginator = s3.get_paginator("list_objects_v2")
-    for page in paginator.paginate(Bucket=bucket, prefix=prefix):
+    client = s3_client or boto3.client("s3")
+    paginator = client.get_paginator("list_objects_v2")
+
+    for page in paginator.paginate(Bucket=bucket, Prefix=prefix):
         for obj in page.get("Contents", []):
             key = obj["Key"]
-            if key.ends_with(extentions):
+            if key.endswith(extensions):
                 yield key
